c2b352cc1462a99fe55e57c7861760e559e4d521
nihalcoskun <nihalcoskkun@gmail.com>
2022-06-10 01:33:36 +0300

    Get Next Line

---
+++ get-next-line.h
@@ -0,0 +1,21 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
+int		ft_find_new_line(char *all_first_left_last);
+size_t	ft_strlen(char *all_first_left_last);
+char	*ft_strjoin(char *buff, char *all_first_left_last);
+char	*ft_with_new_line(int fd, char *all_first_left_last);
+char	*ft_until_new_line(char *all_first_left_last);
+char	*ft_left(char *all_first_left_last);
+char	*get_next_line(int fd);
+
+#endif
